f790be01c04712d1bb88b8a188bd2f63e8493c0d
kartikey-thoughtwin <kartikey@thoughtwin.com>
2024-08-02 15:12:25 +0530

    docker_setup

---
+++ hiretrack/user/models.py
@@ -0,0 +1,9 @@
+# Create your models here.
+from django.db import models
+
+class MyModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
