b79a9a25f629b7477078e215f329fc2ead8d3537
NainazKhan-TW <nainaz@thoughtwin.com>
2024-08-07 15:24:54 +0530

    correct code

--- hiretrack/hiretrack/settings.py
+++ hiretrack/hiretrack/settings.py
@@ -168,8 +168,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=50),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=50),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
     'ALGORITHM': 'HS256',
