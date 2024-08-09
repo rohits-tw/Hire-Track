7cafbcbed32f84d746f4da81824022e65893149b
Rohit-Tw <rohits@thoughtwin.com>
2024-08-09 13:02:50 +0530

    Black formating

--- hiretrack/user/serializers.py
+++ hiretrack/user/serializers.py
@@ -1,11 +1,10 @@
 from rest_framework import serializers
 from .models import CustomUser, UserDetail
 from django.contrib.auth import authenticate
-from phonenumber_field.serializerfields import PhoneNumberField
 from .models import CustomUser
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
-from phonenumber_field.serializerfields import PhoneNumberField 
+from phonenumber_field.serializerfields import PhoneNumberField
 
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -13,9 +12,9 @@ class RegisterSerializer(serializers.ModelSerializer):
     Serializer for user registration. It includes email, password, and username fields.
     Validates the uniqueness of the email and ensures password confirmation.
     """
+
     email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=CustomUser.objects.all())]
+        required=True, validators=[UniqueValidator(queryset=CustomUser.objects.all())]
     )
     password = serializers.CharField(
         write_only=True, required=True, validators=[validate_password]
@@ -24,13 +23,13 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'password', 'password2', 'email')
+        fields = ("username", "password", "password2", "email")
 
     def validate(self, attrs):
         """
         Validate that both password fields match.
         """
-        if attrs['password'] != attrs['password2']:
+        if attrs["password"] != attrs["password2"]:
             raise serializers.ValidationError(
                 {"password": "Password fields didn't match."}
             )
@@ -41,10 +40,9 @@ class RegisterSerializer(serializers.ModelSerializer):
         Create and return a new user instance, given the validated data.
         """
         user = CustomUser.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email']
+            username=validated_data["username"], email=validated_data["email"]
         )
-        user.set_password(validated_data['password'])
+        user.set_password(validated_data["password"])
         user.save()
         return user
 
@@ -54,6 +52,7 @@ class LoginSerializer(serializers.Serializer):
     Serializer for user login. It includes email, username, or phone number fields along with the password.
     Validates that at least one of email, username, or phone number is provided.
     """
+
     email = serializers.EmailField(required=False)
     username = serializers.CharField(required=False)
     phone_number = PhoneNumberField(required=False)
@@ -64,42 +63,63 @@ class LoginSerializer(serializers.Serializer):
         Validate that at least one of email, username, or phone number is provided,
         and check the credentials for authentication.
         """
-        email = data.get('email')
-        username = data.get('username')
-        phone_number = data.get('phone_number')
-        password = data.get('password')
+        email = data.get("email")
+        username = data.get("username")
+        phone_number = data.get("phone_number")
+        password = data.get("password")
 
         if not email and not username and not phone_number:
-            raise serializers.ValidationError('Email, username, or phone number is required to login.')
+            raise serializers.ValidationError(
+                "Email, username, or phone number is required to login."
+            )
 
         user = None
         if email:
-            user = authenticate(request=self.context.get('request'), email=email, password=password)
+            user = authenticate(
+                request=self.context.get("request"), email=email, password=password
+            )
         elif username:
-            user = authenticate(request=self.context.get('request'), username=username, password=password)
+            user = authenticate(
+                request=self.context.get("request"),
+                username=username,
+                password=password,
+            )
         elif phone_number:
-            user = authenticate(request=self.context.get('request'), phone_number=phone_number, password=password)
+            user = authenticate(
+                request=self.context.get("request"),
+                phone_number=phone_number,
+                password=password,
+            )
 
         if not user:
-            raise serializers.ValidationError('Invalid credentials')
+            raise serializers.ValidationError("Invalid credentials")
 
-        data['user'] = user
+        data["user"] = user
         return data
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'phone_number', 'is_active', 'is_staff']
+        fields = ["id", "username", "email", "phone_number", "is_active", "is_staff"]
 
 
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserDetail
         fields = [
-            'id', 'firstname', 'lastname', 'fullname', 
-            'role', 'gender', 'profile_picture', 'address', 
-            'created_at', 'updated_at', 'created_by', 'updated_by'
+            "id",
+            "firstname",
+            "lastname",
+            "fullname",
+            "role",
+            "gender",
+            "profile_picture",
+            "address",
+            "created_at",
+            "updated_at",
+            "created_by",
+            "updated_by",
         ]
 
     # def get_user(self, obj):
@@ -109,22 +129,39 @@ class UserDetailSerializer(serializers.ModelSerializer):
 
 class GetUserSerializers(serializers.ModelSerializer):
     user_detail = UserDetailSerializer(read_only=True)
+
     class Meta:
         model = CustomUser
-        fields = ['id', 'email', 'phone_number','user_detail']
+        fields = ["id", "email", "phone_number", "user_detail"]
 
 
 class AddUserDetailSerializers(serializers.ModelSerializer):
     class Meta:
         model = UserDetail
         fields = [
-            'id','firstname', 'lastname', 'fullname', 'role', 'gender', 'profile_picture', 'address','created_at', 'updated_at', 'created_by', 'updated_by'
+            "id",
+            "firstname",
+            "lastname",
+            "fullname",
+            "role",
+            "gender",
+            "profile_picture",
+            "address",
+            "created_at",
+            "updated_at",
+            "created_by",
+            "updated_by",
         ]
+
+
 class UpdateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserDetail
         fields = [
-            'firstname', 'lastname', 'profile_picture', 
-            'address', 'gender','fullname'
+            "firstname",
+            "lastname",
+            "profile_picture",
+            "address",
+            "gender",
+            "fullname",
         ]
-
--- hiretrack/user/views.py
+++ hiretrack/user/views.py
@@ -1,21 +1,29 @@
-from rest_framework import generics,status,permissions
-from .models import  UserDetail, CustomUser
-from .serializers import UserDetailSerializer,LoginSerializer, RegisterSerializer, GetUserSerializers,UpdateUserSerializer, AddUserDetailSerializers
+from rest_framework import generics, status, permissions
+from .models import UserDetail, CustomUser
+from .serializers import (
+    UserDetailSerializer,
+    LoginSerializer,
+    RegisterSerializer,
+    GetUserSerializers,
+    UpdateUserSerializer,
+    AddUserDetailSerializers,
+)
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
-from .query import get_all_user,get_user
+from .query import get_all_user, get_user
 
 
 class RegisterUserAPIView(generics.CreateAPIView):
     """API view for registering a new user.
 
-        This view handles user registration by accepting user data, validating it,
-        and creating a new user. Upon successful registration, it returns a response
-        containing the user's data along with JWT tokens (refresh and access).
+    This view handles user registration by accepting user data, validating it,
+    and creating a new user. Upon successful registration, it returns a response
+    containing the user's data along with JWT tokens (refresh and access).
     """
+
     permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
 
@@ -24,55 +32,62 @@ class RegisterUserAPIView(generics.CreateAPIView):
         user = serializer.is_valid(raise_exception=True)
         user = serializer.save()
         refresh = RefreshToken.for_user(user)
-        return Response({
+        return Response(
+            {
                 "status": True,
                 "msg": "User created successfully.",
                 "user": serializer.data,
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-            }, status=status.HTTP_201_CREATED)
-      
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            },
+            status=status.HTTP_201_CREATED,
+        )
+
 
 class LoginAPIView(generics.GenericAPIView):
     """
     Handles user login and returns a refresh and access token.
     """
+
     serializer_class = LoginSerializer
     permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
         """
         Processes the login request and returns tokens if valid.
-        
+
         :param request: The request object containing the user credentials.
         :return: A response containing the refresh and access tokens if the credentials are valid.
         """
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
+        user = serializer.validated_data["user"]
         refresh = RefreshToken.for_user(user)
-        return Response({
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-        }, status=status.HTTP_200_OK)
-    
+        return Response(
+            {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            },
+            status=status.HTTP_200_OK,
+        )
 
-  
+
 class LogoutAPIView(generics.GenericAPIView):
     """
     Handles user logout by blacklisting the refresh token.
     """
+
     permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
         """
         Processes the logout request by blacklisting the refresh token.
-        
+
         :param request: The request object containing the refresh token.
         :return: A response indicating the logout status.
         """
         try:
-            refresh_token = request.data['refresh_token']
+            refresh_token = request.data["refresh_token"]
             token = RefreshToken(refresh_token)
             token.blacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
@@ -85,18 +100,23 @@ class GetUser(APIView):
     API view to retrieve user(s). If an ID is provided, retrieves a specific user.
     Otherwise, retrieves all users.
     """
+
     def get(self, request, id=None):
         if id:
             user = get_user(id)
             if not user:
                 return Response({"status": False, "msg": "User does not exist"})
             serializer = GetUserSerializers(user, many=False)
-            return Response({"status": True, "msg": "User fetched.", "data": serializer.data})
+            return Response(
+                {"status": True, "msg": "User fetched.", "data": serializer.data}
+            )
 
         all_users = get_all_user()
         serializer = GetUserSerializers(all_users, many=True)
-        return Response({"status": True, "msg": "Users fetched.", "data": serializer.data})
-    
+        return Response(
+            {"status": True, "msg": "Users fetched.", "data": serializer.data}
+        )
+
 
 class UpdateUserDetailView(generics.UpdateAPIView):
     """
@@ -105,6 +125,7 @@ class UpdateUserDetailView(generics.UpdateAPIView):
     This view allows an authenticated user to update their own profile details,
     including fields like `firstname`, `lastname`, `profile_picture`, `address`, etc.
     """
+
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = UpdateUserSerializer
 
@@ -124,13 +145,17 @@ class UpdateUserDetailView(generics.UpdateAPIView):
         """
         try:
             user_detail = self.get_object()
-            serializer = self.serializer_class(user_detail, data=request.data, partial=True)
+            serializer = self.serializer_class(
+                user_detail, data=request.data, partial=True
+            )
             if serializer.is_valid():
-                serializer.save(updated_by=request.user) 
+                serializer.save(updated_by=request.user)
                 return Response(serializer.data, status=status.HTTP_200_OK)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
 
 class AddUserDetailView(APIView):
@@ -140,14 +165,23 @@ class AddUserDetailView(APIView):
     This view handles POST requests to add user details for the logged-in user. 
     It checks if user details already exist and either saves the new details or returns an error.
     """
+
     def post(self, request):
         user = request.user
         data = request.data
         serializer = AddUserDetailSerializers(data=data)
         if UserDetail.objects.filter(user=user).exists():
-            return Response({"status": False, "msg": "Data Already Exists"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"status": False, "msg": "Data Already Exists"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         if serializer.is_valid():
             serializer.save(user=user)
-            return Response({"status": True, "msg": "Data Saved"}, status=status.HTTP_201_CREATED)
+            return Response(
+                {"status": True, "msg": "Data Saved"}, status=status.HTTP_201_CREATED
+            )
         else:
-            return Response({"status": False, "msg": "Data not saved", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"status": False, "msg": "Data not saved", "errors": serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
