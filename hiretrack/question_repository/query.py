140589098dfce40428afdc70707f86ab2205bfc1
Rishi <rishi@thoughtwin.com>
2024-08-29 10:21:52 +0530

    update_question_by_id

--- hiretrack/question_repository/query.py
+++ hiretrack/question_repository/query.py
@@ -2,5 +2,10 @@ from question_repository.models import QuestionRepository
 
 
 def get_question_by_team_id(id):
-    interview = QuestionRepository.objects.filter(team=id).all()
-    return interview
+    Questions= QuestionRepository.objects.filter(team=id).all()
+    return Questions
+
+
+def get_by_id(id):
+    Question = QuestionRepository.objects.get(id=id)
+    return Question
